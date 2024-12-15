a3b46815979e29bbc03f6af1f10ad64887a50c5d
Anuragc123 <anuragc2077@gmail.com>
2024-12-15 21:59:51 +0530

    minor changes

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+// import "./App.css";
 import { TodoProvider } from "./context";
 import TodoForm from "./components/ToDoForm";
 import TodoItem from "./components/ToDoItem";
@@ -45,8 +45,8 @@ function App() {
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
     >
-      <div className="bg-[#172842] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+      <div className="bg-[#172842] min-h-screen py-8 ">
+        <div className="w-full max-w-xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
